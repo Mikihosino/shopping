6dd70d949455c174d0fb01eed90483f1a781fb33
Mikihosino <89829851+Mikihosino@users.noreply.github.com>
2023-08-20 00:54:26 +0900

    changed cart-item format

--- src/pages/cart/cart-item.jsx
+++ src/pages/cart/cart-item.jsx
@@ -1,31 +1,29 @@
 import React, { useContext } from 'react'
 import './cart-item.css'
 import { ShopContext } from '../../context/shop-context'
+import { Trash } from "phosphor-react";
+
 
 export const CartItem = (props) => {
   const {id, productName, price, productImage} = props.data;
   const {cartItems, addToCart, removeFromCart, updateCartItemCount, deleteCartItem} = useContext(ShopContext);
   return (
     <div className='cartItem'>
-      <div>
       <img src={productImage} />
       <div className='description'> 
         <p><b>
           {productName}</b></p>
         <p> ${price}</p>
       </div>
+      <div className='countHandler'>
+        <button onClick={() => removeFromCart(id)}> - </button>
+        <input value={cartItems[id]} onChange={(e) => updateCartItemCount(Number(e.target.value), id)}/>
+        <button onClick={() => addToCart(id)}> + </button>
       </div>
-      
-      <div className='buttons'>
-        <div className='countHandler'>
-          <button onClick={() => removeFromCart(id)}> - </button>
-          <input value={cartItems[id]} onChange={(e) => updateCartItemCount(Number(e.target.value), id)}/>
-          <button onClick={() => addToCart(id)}> + </button>
+
+      <div className='deleteButton'>
+          <button onClick={() => deleteCartItem(id)}><Trash size={28}/></button>
         </div>
-        <div className='deleteButton'>
-          <button onClick={() => deleteCartItem(id)}> Delete</button>
-        </div>
-      </div>
     </div>
   )
 }
